0eb22a24f16ebe0864c1e0a590d24fd174379661
michael07200 <mickmhtong@gmail.com>
2023-01-21 11:41:08 +0100

    build: Navbar without route

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import Hero from "./components/Hero";
 import Skill from "./components/Skill";
 import Experience from "./components/Experience";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Hero />
       <Skill />
       <Experience />
