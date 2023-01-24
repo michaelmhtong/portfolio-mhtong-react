158d5c976b35e08e37b985a13a6ef81d53d74691
michael07200 <mickmhtong@gmail.com>
2023-01-24 21:19:43 +0100

    build: contact page for mobile

---
+++ src/components/Contact.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="h-52 grid justify-center content-center space-y-2 theme-bg-green text-white text-lg">
+      <p className="font-heading">A wide range of technology?</p>
+      <a href="mailto:mickmhtong@gmail.com">
+        <p className="font-oblique text-center underline">Contact me.</p>
+      </a>
+    </div>
+  );
+};
+
+export default Contact;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ["Gilroy"],
+        oblique: ["LibreBaskerville"],
+        body: ["Lato"],
+      },
+    },
   },
   plugins: [],
 };
