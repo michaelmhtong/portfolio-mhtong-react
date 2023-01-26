fff4d60198ce698c5409062314965010ec5a5a06
michael07200 <mickmhtong@gmail.com>
2023-01-26 19:38:36 +0100

    style: update the color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
         oblique: ["LibreBaskerville"],
         body: ["Lato"],
       },
+      colors: {
+        palm: "#62973d",
+        silver: "#f2f2f2",
+        olive: "41493b",
+      },
     },
   },
   plugins: [],
