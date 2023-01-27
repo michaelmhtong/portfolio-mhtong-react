a09634a07c3caa994d2c7914e3480617a7b91aaa
michael07200 <mickmhtong@gmail.com>
2023-01-27 09:47:45 +0100

    style: skill responsive

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -53,9 +53,9 @@ const Skill = () => {
   const symbol = "/assets/symbol.svg";
 
   return (
-    <div className="px-12">
+    <div className="flex justify-between flex-col px-12 py-24 bg-silver md:px-40 md:py-32 lg:flex-row lg:py-10 xl:px-52">
       {/* text */}
-      <div>
+      <div className="lg:self-center">
         <img src={symbol} alt="/" className="w-10 h-10 cursor-pointer"></img>
         <div>
           <p className="pb-3 font-heading text-palm text-5xl">
@@ -65,7 +65,7 @@ const Skill = () => {
           <p className="pb-2 font-heading text-2xl">
             A wide range of <i className="font-oblique">Web</i> technology<em>.</em>
           </p>
-          <ul className="mt-4 text-xs">
+          <ul className="mt-4 text-xs xl:text-sm">
             <li>
               Proficient: Javascript | React | Tailwind CSS | Node | Express | MongoDB | PostgreSQL
             </li>
@@ -78,48 +78,29 @@ const Skill = () => {
         </div>
       </div>
       {/* iconcloud */}
-      <div className="pt-4">
-        <Cloud>{icons}</Cloud>
+      <div className="pt-4 justify-self-center">
+        <Cloud
+          containerProps={{
+            style: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              maxWidth: "none",
+            },
+            onClick: (event) => event.preventDefault(),
+          }}
+          options={{
+            clickToFront: 500,
+            depth: 1,
+            initial: [0.1, -0.1],
+            outlineColour: "#0000",
+            wheelZoom: false,
+          }}
+        >
+          {icons}
+        </Cloud>
       </div>
     </div>
-
-    // <div className="relative bg-white pt-16 pb-32 overflow-hidden">
-    //   <div>
-    //     <div className="relative">
-    //       <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24 lg: items-center">
-    //         <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
-    //           <div>
-    //             <div className="mt-6">
-    //               <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">Skills</h2>
-    //               {/* As a front-end developer, I have a foundation in web development and am familiar
-    //                 with a range of languages and frameworks, especially in Javascript and React,
-    //                 which allow me to create interactive and responsive user interfaces. To
-    //                 complement my technical skills, I am familiar with various tools such as Figma,
-    //                 Photoshop and Illustrator, which enable me to streamline my workflow and improve
-    //                 efficiency. */}
-    //               <ul className="mt-4 text-base text-gray-500">
-    //                 <li>
-    //                   Proficient: Javascript | React | Tailwind CSS | Node | Express | MongoDB |
-    //                   PostgreSQL
-    //                 </li>
-    //                 <li>Knowledgeable: Typescript | Next.js | Redux | Python | REST API</li>
-    //                 <li>
-    //                   Tools: Git | GitHub | Figma | Postman | Photoshop | Illustrator | Indesign |
-    //                   Grasshopper
-    //                 </li>
-    //               </ul>
-    //             </div>
-    //           </div>
-    //         </div>
-    //         <div className="mt-4">
-    //           <div className="sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-    //             <Cloud>{icons}</Cloud>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
