f81a1557410302d1e1acc430c00fd23c70ce5304
michael07200 <mickmhtong@gmail.com>
2023-01-27 11:01:53 +0100

    style: experience mobile

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -48,49 +48,89 @@ const Experience = () => {
     },
   ];
 
+  const symbol = "/assets/symbol.svg";
+
   return (
-    <div className="relative container mx-auto px-6 flex flex-col space-y-20">
-      {/* TimeLine */}
-      <div className="absolute z-0 w-2 h-full bg-white shadow-md inset-0 left-16 md:mx-auto md:right-0 md:left-0"></div>
-
-      {/* Content */}
-      {experiences.map((experience, index) => (
-        <motion.div
-          className="relative z-10"
-          initial={{ x: index % 2 !== 0 ? 100 : -100, opacity: 0 }} // odd index: x:-100, even index: x:100
-          whileInView={{ x: 0, opacity: 1 }}
-          transition={{ duration: 1.5 }}
-          key={index}
-        >
-          <img
-            src={experience.imageSrc}
-            alt=""
-            className="h-24 w-24 bg-white object-fit-contain rounded-full shadow-md border-4 border-white sm:absolute md:mx-auto md:left-0 md:right-0"
-          />
-          <div
-            className={`${
-              index % 2 !== 0 ? "timeline-container" : "timeline-container timeline-container-left" // even index left
-            }`}
-          >
-            <div
-              className={`${
-                index % 2 !== 0 ? "timeline-pointer" : "timeline-pointer timeline-pointer-left" // even index left
-              }`}
-              aria-hidden="true"
-            ></div>
-
-            <div className="bg-white p-6 rounded-md shadow-md">
-              <span className="font-bold text-indigo-600 text-sm tracking-wide">
-                {experience.startDate} - {experience.endDate}
-              </span>
-              <h1 className="text-2xl font-bold pt-1">{experience.position}</h1>
-              <p className="pt-1">{experience.companyName}</p>
-              <p>{experience.summaryPoints}</p>
-            </div>
+    <div className="flex flex-col px-12 py-16 bg-silver">
+      {/* title */}
+      <div className="text-right">
+        <img src={symbol} alt="/" className="w-10 h-10 cursor-pointer inline-block"></img>
+        <div>
+          <p className="pb-3 font-heading text-palm text-5xl xl:text-6xl">
+            <div className="pb-2">EXP</div>
+            <div>ERIENCE.</div>
+          </p>
+          <p className="pb-2 font-heading text-2xl xl:text-3xl">
+            A wide range of <i className="font-oblique">Web</i> technology
+            <em className="text-palm">.</em>
+          </p>
+        </div>
+      </div>
+      <div className="relative container flex flex-col space-y-10 pb-10">
+        {/* timeline */}
+        <div className="absolute z-0 w-0.5 left-1.5 h-full bg-palm inset-0"></div>
+        {/* content */}
+        {experiences.map((experience, index) => (
+          <div id={index} className="pl-8">
+            {/* point circle */}
+            <span className="absolute left-0 h-4 w-4 rounded-full bg-palm"></span>
+            <span className="font-body text-xs tracking-wide">
+              {experience.startDate} - {experience.endDate}
+            </span>
+            <h1 className="pt-1 font-heading text-2xl text-palm">{experience.position}</h1>
+            <p className="pt-1 font-oblique text-base text-light">{experience.companyName}</p>
+            <ul className="text-sm pt-3">
+              {experience.summaryPoints.map((point, index) => (
+                <li key={index}>{point}</li>
+              ))}
+            </ul>
           </div>
-        </motion.div>
-      ))}
+        ))}
+      </div>
     </div>
+
+    // <div className="relative container mx-auto px-6 flex flex-col space-y-20">
+    //   {/* TimeLine */}
+    //   <div className="absolute z-0 w-2 h-full bg-white shadow-md inset-0 left-16 md:mx-auto md:right-0 md:left-0"></div>
+
+    //   {/* Content */}
+    //   {experiences.map((experience, index) => (
+    //     <motion.div
+    //       className="relative z-10"
+    //       initial={{ x: index % 2 !== 0 ? 100 : -100, opacity: 0 }} // odd index: x:-100, even index: x:100
+    //       whileInView={{ x: 0, opacity: 1 }}
+    //       transition={{ duration: 1.5 }}
+    //       key={index}
+    //     >
+    //       <img
+    //         src={experience.imageSrc}
+    //         alt=""
+    //         className="h-24 w-24 bg-white object-fit-contain rounded-full shadow-md border-4 border-white sm:absolute md:mx-auto md:left-0 md:right-0"
+    //       />
+    //       <div
+    //         className={`${
+    //           index % 2 !== 0 ? "timeline-container" : "timeline-container timeline-container-left" // even index left
+    //         }`}
+    //       >
+    //         <div
+    //           className={`${
+    //             index % 2 !== 0 ? "timeline-pointer" : "timeline-pointer timeline-pointer-left" // even index left
+    //           }`}
+    //           aria-hidden="true"
+    //         ></div>
+
+    //         <div className="bg-white p-6 rounded-md shadow-md">
+    //           <span className="font-bold text-indigo-600 text-sm tracking-wide">
+    //             {experience.startDate} - {experience.endDate}
+    //           </span>
+    //           <h1 className="text-2xl font-bold pt-1">{experience.position}</h1>
+    //           <p className="pt-1">{experience.companyName}</p>
+    //           <p>{experience.summaryPoints}</p>
+    //         </div>
+    //       </div>
+    //     </motion.div>
+    //   ))}
+    // </div>
   );
 };
 
