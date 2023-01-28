7fb077c8e3e89cf51c333ef979e9e3a4252bc24d
michael07200 <mickmhtong@gmail.com>
2023-01-28 11:40:10 +0100

    build: update link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,37 +1,44 @@
 import React from "react";
 import { HiArrowLongRight } from "react-icons/hi2";
+import { ReactComponent as NavLogo } from "../image/logo.svg";
 
 const Footer = () => {
-  const NavLogo = "/assets/logo.svg";
   return (
     <div className="px-14 py-6 bg-silver md:flex md:justify-between md:items-center md:py-12 lg:px-52">
       {/* left */}
-      <div className="hidden md:flex md:items-center">
-        <HiArrowLongRight size={50} />
-        <p className="pl-9">
-          <div className="font-oblique text-base">Interested ?</div>
-          <div className="font-heading text-3xl">MICHAEL TONG.</div>
-        </p>
-      </div>
+      <a href="mailto:mickmhtong@gmail.com">
+        <div className="hidden md:flex md:items-center">
+          <HiArrowLongRight size={50} />
+          <p className="pl-9">
+            <div className="font-oblique text-base">Connect with me</div>
+            <div className="font-heading text-3xl">MICHAEL TONG.</div>
+          </p>
+        </div>
+      </a>
 
       {/* middle */}
       <div className="hidden lg:flex">
         <p className="lg:text-sm">
           <div className="font-heading">mickmhtong@gmail.com</div>
-          <div className="font-body">github | linkedin</div>
+          <div className="font-body">
+            <a href="https://github.com/michaelmhtong" className="underline">
+              github
+            </a>{" "}
+            | {}
+            <a href="https://www.linkedin.com/in/mhtong/" className="underline">
+              linkedin
+            </a>
+          </div>
         </p>
       </div>
 
       {/* right */}
       <div className="md:text-right">
-        <img
-          src={NavLogo}
-          alt="/"
-          className="w-10 h-10 fill-blue-500 cursor-pointer md:inline-block"
-        />
+        <NavLogo className="w-10 h-10 fill-olive cursor-pointer md:inline-block" />
         <p className="text-xs font-body pt-1">
           <div>© 2023 Michael Tong</div>
-          <div>Built with React, Tailwind CSS and Framer Motion</div>
+          <div>Design in Figma</div>
+          <div>Build in React, Tailwind CSS and Framer Motion</div>
         </p>
       </div>
     </div>
