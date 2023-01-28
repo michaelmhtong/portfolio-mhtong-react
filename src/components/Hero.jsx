68d65b81f4ee3cbc2a7e85c91f568ac4957078ae
michael07200 <mickmhtong@gmail.com>
2023-01-28 11:14:01 +0100

    style: update layout with icons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
+import { SiGmail, SiGithub, SiLinkedin } from "react-icons/si";
+import { ReactComponent as NavLogo } from "../image/logo.svg";
+import { HiArrowLongLeft } from "react-icons/hi2";
 
 const Hero = () => {
   const [text, count] = useTypewriter({
@@ -14,65 +17,91 @@ const Hero = () => {
   });
 
   return (
-    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center">
-      <h1></h1>
-      <div>
-        <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">
-          I am Michael Tong
-        </h1>
-        <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
-          I am {text}
-          <Cursor cursorColor="#62973D" />
-        </p>
-        <div className="mt-8 flex gap-x-4 sm:justify-center">
-          <a
-            href="#"
-            className="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
-          >
-            Get started{" "}
-            <span className="text-indigo-200" aria-hidden="true">
-              &rarr;
-            </span>
-          </a>
-          <a
-            href="#"
-            className="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
-          >
-            Live demo{" "}
-            <span className="text-gray-400" aria-hidden="true">
-              &rarr;
-            </span>
-          </a>
+    <div>
+      <section className="flex relative h-screen justify-center align-middle">
+        <div className="w-full bg-palm"></div>
+        <div className="w-full bg-silver"></div>
+        <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] font-heading text-white text-5xl tracking-[0.3em] md:text-6xl lg:text-7xl xl:text-8xl">
+          <p>MICHAEL</p>
+          <p className="pt-2 md:pt-4 lg:pt-6 xl:pt-8">
+            TONG<span className="text-palm">.</span>
+          </p>
         </div>
-      </div>
-      <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
-        <svg
-          className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
-          viewBox="0 0 1155 678"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
-            fillOpacity=".3"
-            d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-          />
-          <defs>
-            <linearGradient
-              id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
-              x1="1155.49"
-              x2="-78.208"
-              y1=".177"
-              y2="474.645"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor="#9089FC" />
-              <stop offset={1} stopColor="#FF80B5" />
-            </linearGradient>
-          </defs>
-        </svg>
-      </div>
+        {/* logo */}
+        <NavLogo className="absolute h-11 top-14 left-10 fill-silver lg:h-14 lg:left-20 xl:h-16 xl:left-24" />
+        {/* social */}
+        <div className="hidden absolute md:flex flex-col h-full justify-center space-y-6 right-10 lg:right-16 lg:space-y-8 xl:right-20 xl:space-y-10">
+          <SiGmail size={20} /> <SiGithub size={20} /> <SiLinkedin size={20} />
+        </div>
+        {/* scroll text */}
+        <div className="absolute rotate-[-90deg] bottom-8 mr-8 text-white text-[8px] md:text-[10px] md:bottom-9 md:mr-12 lg:bottom-12 lg:mr-14 lg:text-xs">
+          <div className="flex justify-center items-center">
+            <HiArrowLongLeft className="mr-2" />
+            Scroll
+          </div>
+        </div>
+      </section>
+      <section></section>
     </div>
+    // <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center">
+    //   <h1></h1>
+    //   <div>
+    //     <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">
+    //       I am Michael Tong
+    //     </h1>
+    //     <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
+    //       I am {text}
+    //       <Cursor cursorColor="#62973D" />
+    //     </p>
+    //     <div className="mt-8 flex gap-x-4 sm:justify-center">
+    //       <a
+    //         href="#"
+    //         className="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
+    //       >
+    //         Get started{" "}
+    //         <span className="text-indigo-200" aria-hidden="true">
+    //           &rarr;
+    //         </span>
+    //       </a>
+    //       <a
+    //         href="#"
+    //         className="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+    //       >
+    //         Live demo{" "}
+    //         <span className="text-gray-400" aria-hidden="true">
+    //           &rarr;
+    //         </span>
+    //       </a>
+    //     </div>
+    //   </div>
+    //   <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+    //     <svg
+    //       className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
+    //       viewBox="0 0 1155 678"
+    //       fill="none"
+    //       xmlns="http://www.w3.org/2000/svg"
+    //     >
+    //       <path
+    //         fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
+    //         fillOpacity=".3"
+    //         d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+    //       />
+    //       <defs>
+    //         <linearGradient
+    //           id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
+    //           x1="1155.49"
+    //           x2="-78.208"
+    //           y1=".177"
+    //           y2="474.645"
+    //           gradientUnits="userSpaceOnUse"
+    //         >
+    //           <stop stopColor="#9089FC" />
+    //           <stop offset={1} stopColor="#FF80B5" />
+    //         </linearGradient>
+    //       </defs>
+    //     </svg>
+    //   </div>
+    // </div>
   );
 };
 
