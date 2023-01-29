ce602c3edae6a5cf4c56de9946b9a2209f75e873
michael07200 <mickmhtong@gmail.com>
2023-01-29 16:30:30 +0100

    fix: scroll timeline bug

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useState } from "react";
 import { motion, easeIn, easeInOut } from "framer-motion";
-import ModalHeading from "./model/ModalHeading";
+import { InView } from "react-intersection-observer";
+import ModalHeading from "./ModalHeading";
 
 const Experience = () => {
   const experiences = [
@@ -47,59 +49,74 @@ const Experience = () => {
     },
   ];
 
+  const [entered, setEntered] = useState(false);
+
   return (
-    <div
-      id="experience"
-      className="overflow-hidden flex flex-col px-12 py-12 bg-silver md:px-32 md:py-20 xl:px-64"
+    <InView
+      as="div"
+      onChange={(inView, entry) => {
+        console.log("Inview:", inView, "entry:", entry);
+        if (inView === true) {
+          setEntered(true);
+        }
+      }}
+      root={null}
+      rootMargin="0px"
+      threshold={0.8}
     >
-      {/* title */}
-      <div className="text-right pb-4">
-        <ModalHeading title="Experience" subtitle="Journey towards * industry" word="Tech" />
-      </div>
-      <div className="relative container flex flex-col space-y-10 pb-10 md:space-y-14">
-        {/* timeline */}
-        <div className="absolute z-0 w-0.5 left-[6.5px] h-full inset-0">
-          <svg width="full" height="full">
-            <motion.line
-              y2="100%"
-              stroke="#62973d"
-              strokeWidth="4"
-              initial={{ pathLength: 0 }}
-              animate={{ pathLength: 1 }}
-              transition={{ ease: easeIn, duration: 2, delay: 1 }}
-            />
-          </svg>
+      <div
+        id="experience"
+        className="overflow-hidden flex flex-col px-12 py-12 bg-silver md:px-32 md:py-20 xl:px-[12%] 2xl:px-[14%] 3xl:px-[18%]"
+      >
+        {/* title */}
+        <div className="text-right pb-4">
+          <ModalHeading title="Experience" subtitle="Journey towards * industry" word="Tech" />
         </div>
-        {/* content */}
-        {experiences.map((experience, index) => (
-          <motion.div
-            id={index}
-            className="pl-8"
-            initial={{ x: 500, opacity: 0 }}
-            whileInView={{ x: 0, opacity: 1 }}
-            transition={{ ease: easeInOut, duration: 1, delay: 1 }}
-            viewport={{ once: true }}
-          >
-            {/* point circle */}
-            <span className="absolute left-0 h-4 w-4 rounded-full bg-palm"></span>
-            <span className="font-body text-xs tracking-wide">
-              {experience.startDate} - {experience.endDate}
-            </span>
-            <h1 className="pt-1 font-heading text-2xl text-palm lg:text-3xl">
-              {experience.position}
-            </h1>
-            <p className="pt-1 font-oblique text-base text-light lg:text-xl">
-              {experience.companyName}
-            </p>
-            <ul className="text-sm pt-3">
-              {experience.summaryPoints.map((point, index) => (
-                <li key={index}>{point}</li>
-              ))}
-            </ul>
-          </motion.div>
-        ))}
+        <div className="relative container flex flex-col space-y-10 pb-10 md:space-y-14">
+          {/* timeline */}
+          <div className="absolute z-0 w-0.5 left-[6.5px] h-full inset-0">
+            <svg width="full" height="full">
+              <motion.line
+                y2="100%"
+                stroke="#62973d"
+                strokeWidth="4"
+                initial={{ pathLength: 0 }}
+                animate={entered === true ? { pathLength: 1 } : { pathLength: 0 }}
+                transition={{ ease: easeIn, duration: 0.8, delay: 0.8 }}
+              />
+            </svg>
+          </div>
+          {/* content */}
+          {experiences.map((experience, index) => (
+            <motion.div
+              id={index}
+              className="overflow-hidden pl-8"
+              initial={{ x: 100, opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{ ease: easeInOut, duration: 0.8, delay: 0.8 }}
+              viewport={{ once: true }}
+            >
+              {/* point circle */}
+              <span className="absolute left-0 h-4 w-4 rounded-full bg-palm"></span>
+              <span className="font-body text-xs tracking-wide xl:text-sm">
+                {experience.startDate} - {experience.endDate}
+              </span>
+              <h1 className="pt-1 font-heading text-2xl text-palm lg:text-3xl xl:text-4xl">
+                {experience.position}
+              </h1>
+              <p className="pt-1 font-oblique text-base text-light lg:text-xl xl:text-2xl">
+                {experience.companyName}
+              </p>
+              <ul className="text-sm pt-3 xl:text-base">
+                {experience.summaryPoints.map((point, index) => (
+                  <li key={index}>{point}</li>
+                ))}
+              </ul>
+            </motion.div>
+          ))}
+        </div>
       </div>
-    </div>
+    </InView>
   );
 };
 
