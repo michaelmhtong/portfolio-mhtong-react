ba7e2a126c8ffaac80eaa0796c292dc308d604dd
michael07200 <mickmhtong@gmail.com>
2023-01-29 11:19:21 +0100

    fix: z-index display

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="h-52 grid justify-center content-center space-y-2 theme-bg-green text-white text-lg md:text-xl md:h-64 lg:h-80 lg:space-y-3 lg:text-2xl">
-      <p className="font-heading">A wide range of technology?</p>
+    <div className="relative z-[1] h-48 grid justify-center content-center space-y-2 bg-palm text-white text-lg md:text-xl md:h-64 lg:h-80 lg:space-y-3 lg:text-2xl">
+      <p className="font-heading">More information?</p>
       <a href="mailto:mickmhtong@gmail.com">
         <p className="font-oblique text-center underline">Contact me.</p>
       </a>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,47 +1,55 @@
 import React from "react";
 import { HiArrowLongRight } from "react-icons/hi2";
 import { ReactComponent as NavLogo } from "../image/logo.svg";
+import { motion, useScroll } from "framer-motion";
 
 const Footer = () => {
   return (
-    <div className="px-14 py-6 bg-silver md:flex md:justify-between md:items-center md:py-12 lg:px-52">
-      {/* left */}
-      <a href="mailto:mickmhtong@gmail.com">
-        <div className="hidden md:flex md:items-center">
-          <HiArrowLongRight size={50} />
-          <p className="pl-9">
-            <div className="font-oblique text-base">Connect with me</div>
-            <div className="font-heading text-3xl">MICHAEL TONG.</div>
+    <motion.div
+      initial={{ y: -200 }}
+      whileInView={{ y: 0 }}
+      transition={{ duration: 1.5 }}
+      viewport={{ once: true }}
+    >
+      <div className="px-14 py-6 bg-silver md:flex md:justify-between md:items-center md:py-12 lg:px-52">
+        {/* left */}
+        <a href="mailto:mickmhtong@gmail.com">
+          <div className="hidden md:flex md:items-center">
+            <HiArrowLongRight size={50} />
+            <p className="pl-9">
+              <div className="font-oblique text-base">Connect with me</div>
+              <div className="font-heading text-3xl">MICHAEL TONG.</div>
+            </p>
+          </div>
+        </a>
+
+        {/* middle */}
+        <div className="hidden lg:flex">
+          <p className="lg:text-sm">
+            <div className="font-heading">mickmhtong@gmail.com</div>
+            <div className="font-body">
+              <a href="https://github.com/michaelmhtong" target="_blank" className="underline">
+                github
+              </a>{" "}
+              | {}
+              <a href="https://www.linkedin.com/in/mhtong/" target="_blank" className="underline">
+                linkedin
+              </a>
+            </div>
           </p>
         </div>
-      </a>
 
-      {/* middle */}
-      <div className="hidden lg:flex">
-        <p className="lg:text-sm">
-          <div className="font-heading">mickmhtong@gmail.com</div>
-          <div className="font-body">
-            <a href="https://github.com/michaelmhtong" className="underline">
-              github
-            </a>{" "}
-            | {}
-            <a href="https://www.linkedin.com/in/mhtong/" className="underline">
-              linkedin
-            </a>
-          </div>
-        </p>
+        {/* right */}
+        <div className="md:text-right">
+          <NavLogo className="w-10 h-10 fill-olive cursor-pointer md:inline-block" />
+          <p className="text-xs font-body pt-1">
+            <div>© 2023 Michael Tong</div>
+            <div>Design in Figma</div>
+            <div>Build in React, Tailwind CSS and Framer Motion</div>
+          </p>
+        </div>
       </div>
-
-      {/* right */}
-      <div className="md:text-right">
-        <NavLogo className="w-10 h-10 fill-olive cursor-pointer md:inline-block" />
-        <p className="text-xs font-body pt-1">
-          <div>© 2023 Michael Tong</div>
-          <div>Design in Figma</div>
-          <div>Build in React, Tailwind CSS and Framer Motion</div>
-        </p>
-      </div>
-    </div>
+    </motion.div>
   );
 };
 
